a746e50a27aeabca91a6f6d0f0192f960291d732
Chetan Krishna Palicherla <chetanpalicherla@gmail.com>
2025-02-02 12:32:26 -0500

    Added Create Operation for all Collections

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -30,6 +30,7 @@ const TaskType = new GraphQLObjectType({
   name: "Task",
   fields: () => ({
     id: { type: GraphQLID },
+    title: { type: GraphQLString },
     description: { type: GraphQLString },
     start_date: { type: GraphQLDate },
     end_date: { type: GraphQLDate },
@@ -74,7 +75,7 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     projects: {
       type: GraphQLList(ProjectType),
-      resolve(parent, args) {        
+      resolve(parent, args) {
         return Project.find();
       },
     },
@@ -86,99 +87,130 @@ const RootQuery = new GraphQLObjectType({
       },
     },
     users: {
-        type: GraphQLList(UserType),
-        resolve(parent, args) {
-            return User.find()
-        }
+      type: GraphQLList(UserType),
+      resolve(parent, args) {
+        return User.find();
+      },
     },
     user: {
-        type: UserType,
-        args: {id: {type: GraphQLID}},
-        resolve(parent, args) {
-            return User.findById(args.id)
-        }
-    }
-    }
+      type: UserType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return User.findById(args.id);
+      },
+    },
+    tasks: {
+      type: GraphQLList(TaskType),
+      resolve(parent, args) {
+        return Task.find();
+      },
+    },
+    task: {
+      type: TaskType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Task.findById(args.id);
+      },
+    },
+  },
 });
 
-
 const mutation = new GraphQLObjectType({
-    name: 'Mutation',
-    fields: {
-        addUser: {
-            type: UserType,
-            args: {
-                name: {
-                    type: GraphQLNonNull(GraphQLString),
-                },
-                email: {
-                    type: GraphQLNonNull(GraphQLString)
-                },
-                skills: {
-                    type: GraphQLNonNull(GraphQLList(GraphQLString))
-                }
-            },
-            resolve(parent, args) {
-                const user = new User({
-                    name: args.name,
-                    email: args.email,
-                    skills: args.skills
-                })
-                return user.save()
-            }
+  name: "Mutation",
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        name: {
+          type: GraphQLNonNull(GraphQLString),
         },
-        addTask: {
-            type: TaskType,
-            args: {
-                title: {
-                    type: GraphQLNonNull(GraphQLString)
-                },
-                description: {
-                    type: GraphQLNonNull(GraphQLString)
-                },
-                start_date: {
-                    type: GraphQLNonNull(GraphQLDate)
-                },
-                end_date: {
-                    type: GraphQLNonNull(GraphQLDate)
-                },
-                dependencies: {
-                    type: GraphQLList(GraphQLID),
-                    defaultValue: []
-                },
-                skills: {
-                    type: GraphQLNonNull(GraphQLList(GraphQLString))
-                },
-                status: {
-                    type: new GraphQLEnumType({
-                        name: "ProjectStatus",
-                        values: {
-                            "new": {value: 'Not Started'}, 
-                            "progress": {value: 'In Progress'}, 
-                            "compelted": {value: 'Completed'}
-                        }
-                    }),
-                    defaultValue: 'Not Started'
-                }
+        email: {
+          type: GraphQLNonNull(GraphQLString),
+        },
+        skills: {
+          type: GraphQLNonNull(GraphQLList(GraphQLString)),
+        },
+      },
+      resolve(parent, args) {
+        const user = new User({
+          name: args.name,
+          email: args.email,
+          skills: args.skills,
+        });
+        return user.save();
+      },
+    },
+    addTask: {
+      type: TaskType,
+      args: {
+        title: {
+          type: GraphQLNonNull(GraphQLString),
+        },
+        description: {
+          type: GraphQLNonNull(GraphQLString),
+        },
+        start_date: {
+          type: GraphQLNonNull(GraphQLDate),
+        },
+        end_date: {
+          type: GraphQLNonNull(GraphQLDate),
+        },
+        dependencies: {
+          type: GraphQLList(GraphQLID),
+          defaultValue: [],
+        },
+        skills: {
+          type: GraphQLNonNull(GraphQLList(GraphQLString)),
+        },
+        status: {
+          type: new GraphQLEnumType({
+            name: "ProjectStatus",
+            values: {
+              new: { value: "Not Started" },
+              progress: { value: "In Progress" },
+              compelted: { value: "Completed" },
             },
-            resolve(parent, args) {
-                const task = new Task({
-                    title: args.title,
-                    description: args.description,
-                    start_date: args.start_date,
-                    end_date: args.end_date,
-                    dependencies: args.dependencies,
-                    skills: args.skills,
-                    status: args.status
-                })
-                return task.save()
-            }
-        }
-    }
-})
-
+          }),
+          defaultValue: "Not Started",
+        },
+      },
+      resolve(parent, args) {
+        const task = new Task({
+          title: args.title,
+          description: args.description,
+          start_date: args.start_date,
+          end_date: args.end_date,
+          dependencies: args.dependencies,
+          skills: args.skills,
+          status: args.status,
+        });
+        return task.save();
+      },
+    },
+    addProject: {
+      type: ProjectType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        description: { type: GraphQLNonNull(GraphQLString) },
+        team_members: { type: GraphQLNonNull(GraphQLList(GraphQLID)) },
+        task_list: { type: GraphQLNonNull(GraphQLList(GraphQLID)) },
+        deadline: { type: GraphQLNonNull(GraphQLDate) },
+      },
+      resolve(parent, args) {
+        const project = new Project({
+          name: args.name,
+          description: args.description,
+          team_members: args.team_members,
+          task_list: args.task_list,
+          deadline: args.deadline,
+        });
+        return project.save();
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery,
-    mutation
-})
+  query: RootQuery,
+  mutation,
+});
